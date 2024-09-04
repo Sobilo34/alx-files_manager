7de0bb3b7576c6bd2362d308ac6339ac4a16638a
Sobilo34 <bilalsolih60@gmail.com>
2024-09-04 23:45:43 +0100

    resolve eslint error

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,41 +1,37 @@
-import dbClient from "../utils/db";
 import crypto from 'crypto';
-
+import dbClient from '../utils/db';
 
 class users {
   static async postNew(request, response) {
-    if (!request.body.hasOwnProperty('email')) 
-    { 
-      response.status(400).json({ error: 'Missing email' }) 
+    if (!request.body.hasOwnProperty.call('email')) {
+      response.status(400).json({ error: 'Missing email' });
     }
 
-    if (!request.body.hasOwnProperty('password'))
-    {
-      response.status(400).json({ error: 'Missing password' })
+    if (!request.body.hasOwnProperty.call('password')) {
+      response.status(400).json({ error: 'Missing password' });
     }
 
     const { email, password } = request.body;
-    const user = await dbClient.db.collection('users').findOne({'email': email})
-    .catch ((err) => console.log(err) );
+    const user = await dbClient.db.collection('users').findOne({ email })
+      .catch((err) => console.log(err));
 
     if (user) {
       response.status('400').json({ error: 'Already exist' });
     } else {
       const hash = crypto.createHash('sha1');
       hash.update(password);
-      const pass_result = hash.digest('hex');
-      
-      const con = await dbClient.db.collection('users').insertOne({'email': email, 'password': pass_result});
+      const pwdResult = hash.digest('hex');
+
+      const con = await dbClient.db.collection('users').insertOne({ email, password: pwdResult });
       console.log(con);
       const { ops } = con;
       const id = ops[0]._id;
 
       if (id) {
-        response.status(201).json({ 'id': id, 'email': email });
+        response.status(201).json({ id, email });
       }
     }
   }
 }
-
 
 export default users;
