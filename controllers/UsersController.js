a60331229855a35ad8a30852d67d9d7c47fb1559
EMarvelM <marvelousegbe19@gmail.com>
2024-09-04 23:16:43 +0100

    Now that we have a simple API, it’s time to add users to our database.
    In the file routes/index.js, add a new endpoint:
    
        POST /users => UsersController.postNew
    
    Inside controllers, add a file UsersController.js that contains the new endpoint:
    
    POST /users should create a new user in DB:
    
        To create a user, you must specify an email and a password
        If the email is missing, return an error Missing email with a status code 400
        If the password is missing, return an error Missing password with a status code 400
        If the email already exists in DB, return an error Already exist with a status code 400
        The password must be stored after being hashed in SHA1
        The endpoint is returning the new user with only the email and the id (auto generated by MongoDB) with a status code 201
        The new user must be saved in the collection users:
            email: same as the value received
            password: SHA1 value of the value received

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,19 +1,41 @@
 import dbClient from "../utils/db";
+import crypto from 'crypto';
+
 
 class users {
-	static async postNew(request, response) {
-		console.log(request.body);
-		if (!request.body.hasOwnProperty('email')) { response.status('400').json({error: 'Missing email'}) }
-		if (!request.body.hasOwnProperty('password')) { response.status('400').json({error: 'Missing password'}) }
+  static async postNew(request, response) {
+    if (!request.body.hasOwnProperty('email')) 
+    { 
+      response.status(400).json({ error: 'Missing email' }) 
+    }
 
-		const { email } = request.body;
-		const tp = await dbClient.db.collection('users').findOne({'email': email})
-			.catch ((err) => console.log('--------------------------------------------------\n', err) );
+    if (!request.body.hasOwnProperty('password'))
+    {
+      response.status(400).json({ error: 'Missing password' })
+    }
 
-		if (tp) {
-			response.status('400').json({ error: 'Already exist' });
-		}
-	}
+    const { email, password } = request.body;
+    const user = await dbClient.db.collection('users').findOne({'email': email})
+    .catch ((err) => console.log(err) );
+
+    if (user) {
+      response.status('400').json({ error: 'Already exist' });
+    } else {
+      const hash = crypto.createHash('sha1');
+      hash.update(password);
+      const pass_result = hash.digest('hex');
+      
+      const con = await dbClient.db.collection('users').insertOne({'email': email, 'password': pass_result});
+      console.log(con);
+      const { ops } = con;
+      const id = ops[0]._id;
+
+      if (id) {
+        response.status(201).json({ 'id': id, 'email': email });
+      }
+    }
+  }
 }
+
 
 export default users;
