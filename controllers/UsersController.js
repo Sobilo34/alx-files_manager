8006b1b574e6360813fd8e12ff36b21d298229d8
EMarvelM <marvelousegbe19@gmail.com>
2024-09-05 00:21:04 +0100

    handle edge cases

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,30 +5,28 @@ class users {
   static async postNew(request, response) {
     if (!('email' in request.body)) {
       response.status(400).json({ error: 'Missing email' });
-    }
-
-    if (!('password' in request.body)) {
+    } else if (!('password' in request.body)) {
       response.status(400).json({ error: 'Missing password' });
-    }
-
-    const { email, password } = request.body;
-    const user = await dbClient.db.collection('users').findOne({ email })
-      .catch((err) => console.log(err));
-
-    if (user) {
-      response.status('400').json({ error: 'Already exist' });
     } else {
-      const hash = crypto.createHash('sha1');
-      hash.update(password);
-      const pwdResult = hash.digest('hex');
+      const { email, password } = request.body;
+      const user = await dbClient.db.collection('users').findOne({ email })
+        .catch((err) => console.log(err));
 
-      const con = await dbClient.db.collection('users').insertOne({ email, password: pwdResult });
-      console.log(con);
-      const { ops } = con;
-      const id = ops[0]._id;
+      if (user) {
+        response.status('400').json({ error: 'Already exist' });
+      } else {
+        const hash = crypto.createHash('sha1');
+        hash.update(password);
+        const pwdResult = hash.digest('hex');
 
-      if (id) {
-        response.status(201).json({ id, email });
+        const con = await dbClient.db.collection('users').insertOne({ email, password: pwdResult });
+        console.log(con);
+        const { ops } = con;
+        const id = ops[0]._id;
+
+        if (id) {
+          response.status(201).json({ id, email });
+        }
       }
     }
   }
