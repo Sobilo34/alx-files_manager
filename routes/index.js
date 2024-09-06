d5f6b947c3b2ab23b4ef8c2e954664903ae59cad
Sobilo34 <bilalsolih60@gmail.com>
2024-09-06 10:39:42 +0100

    revert back to initial

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,16 @@
+import express, { Router } from 'express';
 import AppController from '../controllers/AppController';
-import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 
-function routes(app) {
-  app.post('/users', UsersController.postNew);
-  app.put('/files/:id/publish', FilesController.putPublish);
-  app.put('/files/:id/unpublish', FilesController.putUnpublish);
-  app.get('/files/:id', FilesController.getShow);
-  app.get('/connect', AuthController.getConnect);
-  app.get('/disconnect', AuthController.getDisconnect);
-  app.get('/users/me', UsersController.getMe);
-  app.post('/files', FilesController.postUpload);
-  app.get('/status', AppController.getStatus);
-  app.get('/files', FilesController.getIndex);
-  app.get('/stats', AppController.getStats);
-  app.get('/files/:id/data', FilesController.getFile);
-}
+const router = Router();
 
-module.exports = routes;
-export default routes;
+router.use(express.json());
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+
+export default router;
