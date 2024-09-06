851f79799e0949a2c342ef17c72dd3275dc770d6
EMarvelM <marvelousegbe19@gmail.com>
2024-09-06 11:47:32 +0100

    remove multiple import

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 import express, { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-import UserController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 
 const router = Router();
@@ -10,9 +9,8 @@ router.use(express.json());
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UserController.getMe);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
